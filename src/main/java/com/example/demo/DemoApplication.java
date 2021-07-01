e16c584d1f464134db214d64b7ab63a7522aeeeb
Saidugari Prashanth <sprashanth@nisum.com>
2021-07-01 22:51:41 +0530

    new-branch-commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 //shss
+//saddnsd
+//ssfdfd
+//1sdsd
+//ssdsdsd
+//sdffd
+//dfdffdf
+//adad
+//dffd
+//ssdsdsd
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
