e7aa806d5c047aeb61ba935ae55fe01cd62503d0
Saidugari Prashanth <sprashanth@nisum.com>
2021-07-02 11:01:56 +0530

    Hello prashanth

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,16 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-//shss
-//saddnsd
-//ssfdfd
-//1sdsd
-//ssdsdsd
-//sdffd
-//dfdffdf
-//adad
-//dffd
-//ssdsdsd
+//well come prashanth saidugari
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
